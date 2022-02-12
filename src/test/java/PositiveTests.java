41bca637c89669e9bd7f1053db08e627e4388ddb
Weilson1 <galavievai@gmail.com>
2022-02-12 21:31:25 +0300

    Добавлены тесты

---
+++ src/test/java/PositiveTests.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("bot")
+public class PositiveTests {
+
+    @Test
+    void test3(){
+        assertTrue(true);
+    }
+
+    @Test
+    void test4(){
+        assertTrue(true);
+    }
+}
